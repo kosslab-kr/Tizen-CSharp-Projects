5eec3ce2466042e8b46279668d42f430b75f25f5
freebear <ahddl38@gmail.com>
2018-10-05 15:33:08 +0900

    미로찾기게임

---
+++ Find_maze/Find_maze/App.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Tizen.Wearable.CircularUI.Forms;
+
+namespace Find_maze
+{
+    public class App : Application
+    {
+        public App()
+        {
+            Label label = new Label
+            {
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = "미로찾기를 시작합니다."
+            };
+
+            Button button =
+                        new Button
+                        {
+                            Text = "TOUCH",
+                            FontSize = 7,
+                            WidthRequest = 110,
+                            HorizontalOptions = LayoutOptions.Center
+                        };
+            button.Clicked += new EventHandler(button_Clicked);
+
+            // The root page of your application
+            MainPage = new CirclePage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        label, button
+                    }
+                }
+            };
+        }
+
+        private void button_Clicked(object sender, EventArgs e)
+        {
+            NavigationPage navigationPage = new NavigationPage(new views.SubPage());
+            MainPage = navigationPage;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Find_maze/Find_maze/views/SubPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Find_maze.views
+{
+    public class SubPage : ContentPage
+    {
+        public SubPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
